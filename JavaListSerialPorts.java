8fb3e1c37ecede954587eea16eb2e8591903c7c1
Frank Mock <FMock@users.noreply.github.com>
2016-02-20 11:43:13 -0800

    Added files via upload

---
+++ JavaListSerialPorts.java
@@ -0,0 +1,14 @@
+/*
+* This program uses Java Simple Serial Connector library.
+* The official repo for this library is at:
+* https://code.google.com/p/java-simple-serial-connector/
+* One way to use this library is to copy the jssc.jar file
+* to your java library. For example, I created an ext
+* directory in my java lib directory and put jssc.jar in
+* this directory. Then add the library to the build path
+* for each program that uses this library by going to - 
+* Project\Properties\Java Build Path
+* On the Libraries tab click Add External Jars and browes to
+* where the jssc.jar folder is located.
+*/
+import jssc.SerialPortList;
