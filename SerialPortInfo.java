210a7716613e1e3bf9bbce6e136654b7b2dbeae5
Frank Mock <FMock@users.noreply.github.com>
2016-03-16 15:51:01 -0700

    A user can now change the serial port settings.

--- SerialPortInfo.java
+++ SerialPortInfo.java
@@ -1,30 +1,45 @@
 //The object that holds current serial port info
 public class SerialPortInfo
 {
-	private String commPort;
-	private int baudrate, databits, stopbits, parity;
+	/**
+	 * Frank Mock
+	 * Last Updated March 16, 2016
+	 * 
+	 * This class holds the current values of a Serial Port's parameters
+	 */
+	private String comPort;
+	private int baudRate, dataBits, stopBits, parity;
 	
 	public SerialPortInfo(String cp, int br, int db, int sb, int p)
 	{
-		commPort = cp;
-		baudrate = br;
-		databits = db;
-		stopbits = sb;
+		comPort = cp;
+		baudRate = br;
+		dataBits = db;
+		stopBits = sb;
 		parity = p;
 	}
 	
-	public String getCommPort(){ return commPort; }
-	public void setCommPort(String s){ commPort = s; }
+	public String getComPort(){ return comPort; }
+	public void setCommPort(String s){ comPort = s; }
 	
-	public int getBaudRate(){ return baudrate; }
-	public void setBaudRate(int i){ baudrate = i; }
+	public int getBaudRate(){ return baudRate; }
+	public void setBaudRate(int i){ baudRate = i; }
 	
-	public int getDataBits(){ return databits; }
-	public void setDataBits(int db){ databits = db; }
+	public int getDataBits(){ return dataBits; }
+	public void setDataBits(int db){ dataBits = db; }
 	
-	public int getStopBits(){ return stopbits; }
-	public void setStopBits(int sb){ stopbits = sb; }
+	public int getStopBits(){ return stopBits; }
+	public void setStopBits(int sb){ stopBits = sb; }
 	
 	public int getParity(){ return parity; }
 	public void setParity(int p){ parity = p; }
+	
+	public String toString()
+	{
+		return "COM Port = " + comPort + System.getProperty("line.separator") +
+			   "Baud Rate = " + baudRate + System.getProperty("line.separator") +
+			   "Data Bits = " + dataBits + System.getProperty("line.separator") +
+			   "Stop Bits = " + stopBits + System.getProperty("line.separator") +
+			   "Parity = " + parity + System.getProperty("line.separator");
+	}
 }
